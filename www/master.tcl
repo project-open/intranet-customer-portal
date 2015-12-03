706535c2e7cf3ca67db4a1dd35f3db0eb47e136d
Frank Bergmann <frank.bergmann@project-open.com>
2015-12-03 11:48:42 +0000

    - Fixed Gustaf substitution issue

--- www/master.tcl
+++ www/master.tcl
@@ -1,6 +1,7 @@
 # /packages/intranet-core/www/master.tcl
 
 if { ![info exists header_stuff] } { set header_stuff {} }
+if { ![info exists title] && [info exists doc(title)]} { set title $doc(title) }
 if { ![info exists title] } { set title {} }
 if { ![info exists main_navbar_label] } { set main_navbar_label {} }
 if { ![info exists sub_navbar] } { set sub_navbar {} }
